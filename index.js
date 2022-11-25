1c2e17be8ad4f0951190ab52fa080c0b51f1c404
MdYeasinSamadArnob <yeasinsamadarnob@gmail.com>
2022-11-25 13:49:14 +0600

    donee

--- index.js
+++ index.js
@@ -33,7 +33,9 @@ function verifyJWT(req, res, next) {
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b8rim.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b8rim.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b8rim.mongodb.net/test`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
